6769b7ee8b4269f195594de78b9a55bf9f565dcc
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-22 21:08:31 +0530

    commit

--- question_02.py
+++ question_02.py
@@ -1,1 +1,15 @@
-print("Hello")
+# Count of Digits
+
+try:
+    num = int(input("Enter any number: "))
+except:
+    print("Enter positive numbers only")
+    num = int(input("Enter any number: "))
+
+count = 0
+
+while(num > 0):
+    num = num // 10
+    count += 1
+
+print(count)

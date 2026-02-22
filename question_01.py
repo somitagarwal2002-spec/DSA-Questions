78c917e4aa88ccea449010230713b6db6fda8074
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-22 20:54:20 +0530

    commit

---
+++ question_01.py
@@ -0,0 +1,15 @@
+# Printing Digits of a Number from ones place
+
+try:
+    num = int(input("Enter any number: "))
+except:
+    print("Enter integers only")
+    num = int(input("Enter any number: "))
+
+while(num > 0):
+    last_digit = num % 10
+    print(last_digit)
+    num = num // 10
+    
+
+    

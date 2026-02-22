bd4fbf04535c980739ac8000cf14009584396d50
Somit Agarwal <somitagarwal@Somits-MacBook-Air.local>
2026-02-22 21:14:13 +0530

    commit

--- question_02.py
+++ question_02.py
@@ -1,15 +1,21 @@
 # Count of Digits
 
+from math import *
 try:
     num = int(input("Enter any number: "))
 except:
     print("Enter positive numbers only")
     num = int(input("Enter any number: "))
 
+n = num
 count = 0
 
-while(num > 0):
-    num = num // 10
+while(n > 0):
+    n = n // 10
     count += 1
 
 print(count)
+
+# Short Method and More efficient
+
+print(int(log10(num)+1))

c56b8600449228506073ebe1a984674b93d53401
Somit Agarwal <somitagarwal@MacBookAir.lan>
2026-03-20 09:16:51 +0530

    commit

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,17 +1,34 @@
 # Fibonacci Sequence
 
-num = int(input("Enter how many fibonacci numbers you want to see: "))
+# num = int(input("Enter how many fibonacci numbers you want to see: "))
 
-n = num 
+# n = num 
 
-numbers = [1, 1]
-i = 2
+# numbers = [1, 1]
+# i = 2
 
-while(i < n):
-    numbers.append(numbers[i - 1] + numbers[i - 2])
-    i += 1
+# while(i < n):
+#     numbers.append(numbers[i - 1] + numbers[i - 2])
+#     i += 1
 
-# print(numbers[num - 1])
+# # print(numbers[num - 1])
 
-print(numbers)
+# print(numbers)
 
+
+
+
+
+# Using Recursion
+
+def fib(n):
+    if n == 1 or n == 0:
+        return n
+    return fib(n-1) + fib(n-2)
+
+num = int(input("Enter any positive number: "))
+answer = fib(num)
+print(answer)
+
+# Time Complexity = O(2^N)
+# Space Complexity = O(2^N)
--- palindrome_string.py
+++ palindrome_string.py
@@ -1,5 +1,6 @@
 # Palindrome String
 
+# Using For Loop
 string = input("Enter any word: ").strip()
 
 new_string = string
@@ -8,9 +9,49 @@ result = ""
 for i in range(len(new_string) - 1, -1, -1):
     result = result + new_string[i]
 
-# print(result)
-
 if result == new_string:
     print("Palindrome String")
 else:
     print("Not a Palindrome String")
+
+
+
+
+
+# Using Recursion
+
+def check(a, b, result):
+    if b < a:
+        return result
+    result = result + string[b]
+    return check(a, b-1, result)
+
+first = 0
+last = len(string) - 1
+result = ""
+og = check(first, last, result)
+
+if og == string:
+    print("Palindrome String")
+else:
+    print("Not a Palindrome String")
+
+
+
+
+# Using 2 pointer
+string = input("Enter any word: ").strip()
+
+l = 0
+r = len(string) - 1
+
+def check(a, b):
+    if a >= b:
+        return "Palindrome String"
+    elif string[a] == string[b]:
+        return check(a+1, b-1)
+    elif string[a] != string[b]:
+        return "Not a Palindrome String"
+
+result = check(l, r) 
+print(result) 
